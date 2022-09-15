9c95d138531018a83d29396c6633345ebdc1b42e
bhambleton <brianthomashambleton@gmail.com>
2022-09-15 14:21:49 +0000

    adds cna scripts for server, moves cobalt startup to script, updates setup script

---
+++ src/cobalt/entrypoint.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+C2_IP=$1
+C2_PASSWORD=$2
+C2_PROFILE=$3
+KILLDATE=$4
+
+cd /opt/cobaltstrike
+
+(sleep 10 && \
+  ./agscript 172.19.0.5 50050 Alpha_5 ${C2_PASSWORD} cnas/megazord.cna) &
+
+echo "Teamserver starting...";
+
+./teamserver ${C2_IP} ${C2_PASSWORD} ${C2_PROFILE} ${KILLDATE} 
