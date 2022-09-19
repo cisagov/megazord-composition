cf81741f436c5166762f6cb6c1f2e71071af745a
bhambleton <brianthomashambleton@gmail.com>
2022-09-19 15:37:00 -0400

    adds comments, fix linting errors

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-
 function usage {
-    echo -e "Megazord setup script\n"
-    echo "This script will perform setup tasks to initialize the environment\ 
-prior to launching megazord." 
-    echo -e "\nThese tasks include:"
-    echo "    - Setting values in the .env file used by docker compose"
-    echo "    - Generating a pseudorandom string for the uploads directory"
-    echo "    - Extracting certificates and private key from the keystore" 
-    echo "    - Generating a fresh C2 Profile and outputting it in the correct location"
-    echo "    - Generating an htaccess config file for Apache and outputing it in the correct location"
-    echo "    - Generating a fresh Corefile and outputting it in the correct location"
-    echo
-    echo "Options:"
-    echo "    -r    The url Apache will redirect out-of-scope traffic to"
-    echo "    -d    The domain of the teamserver"
-    echo "    -c    The cloudfront url"
-    echo
-    echo "Usage:"
-    echo "    ./setup.sh -r redirect-url.com -d c2domain.com -c cloudfront.domain.net"
+  echo -e "Megazord setup script\n"
+  echo "This script will perform setup tasks to initialize the environment\
+prior to launching megazord."
+  echo -e "\nThese tasks include:"
+  echo "    - Setting values in the .env file used by docker compose"
+  echo "    - Generating a pseudorandom string for the uploads directory"
+  echo "    - Extracting certificates and private key from the keystore"
+  echo "    - Generating a fresh C2 Profile and outputting it in the correct location"
+  echo "    - Generating an htaccess config file for Apache and outputing it in the correct location"
+  echo "    - Generating a fresh Corefile and outputting it in the correct location"
+  echo
+  echo "Options:"
+  echo "    -r    The url Apache will redirect out-of-scope traffic to"
+  echo "    -d    The domain of the teamserver"
+  echo "    -c    The cloudfront url"
+  echo
+  echo "Usage:"
+  echo "    ./setup.sh -r redirect-url.com -d c2domain.com -c cloudfront.domain.net"
 }
 
 # unset variables in case this script is being ran a 2nd time
@@ -35,7 +34,6 @@ original_profile="/tools/Malleable-C2-Profiles/normal/amazon.profile"
 
 #########################################################################
 
-
 # COLORS
 RED_FG="\033[1;31m"
 GREEN_FG="\033[1;32m"
@@ -46,42 +44,47 @@ RESET="\033[0m"
 
 #### END variables ####
 
-
+# Get command-line arguments
 while getopts "r:d:c:h:" arg; do
-    case ${arg} in
-        h)
-            usage
-            exit;;
-        r)
-            redirect_location=${OPTARG};;
-        d)
-            domain=${OPTARG};;
-        c)
-            cloudfront_domain=${OPTARG};;
-        :)
-            echo -e "${RED_FG}[!]${RESET} $0: A value is required for -${OPTARG}" >&2
-            exit 1;;
-        ?)
-            echo -e "${RED_FG}[!]${RESET} $0: Invalid option: -${OPTARG}" >&2
-            exit 2;;
-    esac
+  case ${arg} in
+    h)
+      usage
+      exit
+      ;;
+    r)
+      redirect_location=${OPTARG}
+      ;;
+    d)
+      domain=${OPTARG}
+      ;;
+    c)
+      cloudfront_domain=${OPTARG}
+      ;;
+    :)
+      echo -e "${RED_FG}[!]${RESET} $0: A value is required for -${OPTARG}" >&2
+      exit 1
+      ;;
+    ?)
+      echo -e "${RED_FG}[!]${RESET} $0: Invalid option: -${OPTARG}" >&2
+      exit 2
+      ;;
+  esac
 done
 
 if [ -z "${redirect_location}" ]; then
-    echo -e "${RED_FG}[!]${RESET}-r option is required" >&2
-    exit 1
+  echo -e "${RED_FG}[!]${RESET}-r option is required" >&2
+  exit 1
 fi
 
 if [ -z "${domain}" ]; then
-    echo -e "${RED_FG}[!]${RESET}-d option is required" >&2
-    exit 1
+  echo -e "${RED_FG}[!]${RESET}-d option is required" >&2
+  exit 1
 fi
 
 if [ -z "${cloudfront_domain}" ]; then
-    echo -e "${RED_FG}[!]${RESET}-c option is required" >&2
-    exit 1
+  echo -e "${RED_FG}[!]${RESET}-c option is required" >&2
+  exit 1
 fi
-
 
 # DO NOT modify the following variables
 keystore_password=$(tail -4 $original_profile | grep 'password' | cut -d ' ' -f 5)
@@ -91,36 +94,34 @@ keystore_path="/tools/Malleable-C2-Profiles/normal/${keystore}"
 c2_profile="${domain}-$(date '+%Y-%m-%d').profile"
 ########################################
 
+find_and_replace() {
+  # Get the original line
+  original_val=$(grep "$1" "$3")
 
-find_and_replace () 
-{
-    original_val=$(grep $1 $3)
-    echo "[*] Updating ${1} variable in ${3}"
+  echo "[*] Updating ${1} variable in ${3}"
 
-    if ! sed -i "s|${original_val}|${1}=${2}|" ${3}; then
-        echo -e "${RED_FG}[ \U2757] Error updating ${1} in ${3}${RESET}"
-        exit 1
-    fi
+  if ! sed -i "s|${original_val}|${1}=${2}|" "${3}"; then
+    echo -e "${RED_FG}[ \U2757] Error updating ${1} in ${3}${RESET}"
+    exit 1
+  fi
 
-    echo -e "${GREEN_FG}[\U2714] Added ${1} to ${3}${RESET}\n"
+  echo -e "${GREEN_FG}[\U2714] Added ${1} to ${3}${RESET}\n"
 }
 
-
 # Add cloudfront_domain to env file
-find_and_replace "CLOUDFRONT_DOMAIN" $cloudfront_domain "/tools/megazord-composition/.env"
+find_and_replace "CLOUDFRONT_DOMAIN" "$cloudfront_domain" "/tools/megazord-composition/.env"
 
 # Add c2_domain to env file
-find_and_replace "C2_DOMAIN" ${domain} "/tools/megazord-composition/.env"
+find_and_replace "C2_DOMAIN" "${domain}" "/tools/megazord-composition/.env"
 
 # Update keystore name in .env
-find_and_replace "KEYSTORE" ${keystore} "/tools/megazord-composition/.env"
-
+find_and_replace "KEYSTORE" "${keystore}" "/tools/megazord-composition/.env"
 
 # Extract certificate for domain from keystore
 echo -e "[*] Extracting ssl certificate"
 
-if ! res=$(keytool -export -alias $domain -keystore $keystore_path \
-  -storepass $keystore_password -rfc \
+if ! res=$(keytool -export -alias "$domain" -keystore "$keystore_path" \
+  -storepass "$keystore_password" -rfc \
   -file "/tools/megazord-composition/src/secrets/cobalt.cert" 2>&1); then
   echo -e "${RED_FG} [ \U2757] $res${RESET}"
   exit 1
@@ -131,7 +132,7 @@ echo -e "${GREEN_FG}[\U2714] $res${RESET}\n"
 # Extract key from keystore
 echo "[*] Extracting key"
 
-if ! openssl pkcs12 -in $keystore_path -passin pass:$keystore_password \
+if ! openssl pkcs12 -in "$keystore_path" -passin pass:"$keystore_password" \
   -nodes -nocerts \
   -out "/tools/megazord-composition/src/secrets/cobalt.key"; then
   echo -e "${RED_FG} [ \U2757] Error extracting key from keystore${RESET}"
@@ -144,8 +145,8 @@ echo -e "${GREEN_FG}[\U2714] Key extracted to\
 # Extract certificate bundle from keystore
 echo "[*] Extracting certificate bundle"
 
-keytool -list -rfc -keystore $keystore_path \
-  -storepass $keystore_password \
+keytool -list -rfc -keystore "$keystore_path" \
+  -storepass "$keystore_password" \
   > /tools/megazord-composition/src/secrets/ca_bundle.crt
 
 echo -e "${GREEN_FG}[\U2714] Bundle extracted to\
@@ -153,7 +154,7 @@ echo -e "${GREEN_FG}[\U2714] Bundle extracted to\
 
 # Copy keystore into cobaltstrike directory
 echo -e "[*] Copying the keystore into /opt/cobaltstrike"
-cp $keystore_path /opt/cobaltstrike/$keystore
+cp "$keystore_path" "/opt/cobaltstrike/$keystore"
 echo -e "${GREEN_FG}[\U2714] Keystore copied into /opt/cobaltstrike${RESET}\n"
 
 # Generate new C2 profile via SourcePoint
@@ -166,7 +167,7 @@ profile_string=$(($(shuf -i 1-10 -n 1) <= 5 ? 5 : 7))
 if ! /tools/SourcePoint/SourcePoint -Host "$cloudfront_domain" \
   -Outfile "/opt/cobaltstrike/$c2_profile" \
   -Injector NtMapViewOfSection -Stage True \
-  -Password $keystore_password -Keystore $keystore \
+  -Password "$keystore_password" -Keystore "$keystore" \
   -Profile $profile_string > /dev/null; then
   echo -e "${RED_FG}[ \U2757] Error generating c2 profile${RESET}"
   exit 1
@@ -176,7 +177,7 @@ echo -e "${GREEN_FG}[\U2714] C2 Profile generated at\
  /opt/cobaltstrike/${c2_profile}${RESET}\n"
 
 # Update C2_PROFILE variable in .env file
-find_and_replace "C2_PROFILE" ${c2_profile} "/tools/megazord-composition/.env"
+find_and_replace "C2_PROFILE" "${c2_profile}" "/tools/megazord-composition/.env"
 
 # Generate new .htaccess based on fresh C2 Profile
 echo "[*] Generating .htaccess based on c2_profile"
@@ -229,5 +230,4 @@ echo -e "${MAGENTA_FG}https://${cloudfront_domain}${endpoint}/NAME_OF_PAYLOAD${R
 echo -e "\nPayload also accessible at ${MAGENTA_FG}https://${domain}${endpoint}/NAME_OF_PAYLOAD${RESET}\n"
 
 # Update PAYLOAD_DIR variable in .env with updated payload directory
-find_and_replace "PAYLOAD_DIR" ${endpoint} "/tools/megazord-composition/.env"
-
+find_and_replace "PAYLOAD_DIR" "${endpoint}" "/tools/megazord-composition/.env"
