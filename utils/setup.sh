2734d1855b2751f9fa74fed384fd6bfdab908a47
bhambleton <brianthomashambleton@gmail.com>
2022-09-19 19:22:08 +0000

    Cleans up setup script, adds command-line args for variables

--- utils/setup.sh
+++ utils/setup.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
-# Variables to modify to fit current environment specifications
-domain=INSERT_DOMAIN
-cloudfront_domain=INSERT_CLOUDFRONT_DOMAIN
-keystore_password=INSERT_KEYSTORE_PASSWORD
-redirect_location=google.com
-#####
 
-# DO NOT modify the following variables
-keystore="${domain}.store"
-keystore_path="/tools/Malleable-C2-Profiles/normal/${keystore}"
-c2_profile="${domain}-$(date '+%Y-%m-%d').profile"
-######
+function usage {
+    echo -e "Megazord setup script\n"
+    echo "This script will perform setup tasks to initialize the environment\ 
+prior to launching megazord." 
+    echo -e "\nThese tasks include:"
+    echo "    - Setting values in the .env file used by docker compose"
+    echo "    - Generating a pseudorandom string for the uploads directory"
+    echo "    - Extracting certificates and private key from the keystore" 
+    echo "    - Generating a fresh C2 Profile and outputting it in the correct location"
+    echo "    - Generating an htaccess config file for Apache and outputing it in the correct location"
+    echo "    - Generating a fresh Corefile and outputting it in the correct location"
+    echo
+    echo "Options:"
+    echo "    -r    The url Apache will redirect out-of-scope traffic to"
+    echo "    -d    The domain of the teamserver"
+    echo "    -c    The cloudfront url"
+    echo
+    echo "Usage:"
+    echo "    ./setup.sh -r redirect-url.com -d c2domain.com -c cloudfront.domain.net"
+}
+
+# unset variables in case this script is being ran a 2nd time
+unset -v redirect_location
+unset -v domain
+unset -v cloudfront_domain
+
+# Ensure the following variable is the correct path
+#########################################################################
+
+# Path to amazon.profile or ocsp.profile containing keystore information
+original_profile="/tools/Malleable-C2-Profiles/normal/amazon.profile"
+
+#########################################################################
+
 
 # COLORS
 RED_FG="\033[1;31m"
@@ -21,44 +44,77 @@ MAGENTA_FG="\033[1;35m"
 #CYAN_FG="\033[1;36m"
 RESET="\033[0m"
 
-# Add cloudfront_domain to env file
+#### END variables ####
 
-echo "[*] Updating cloudfront_domain variable in /tools/megazord-composition/.env"
-original_val=$(grep 'CLOUDFRONT_DOMAIN' /tools/megazord-composition/.env)
 
-if ! sed -i "s|${original_val}|CLOUDFRONT_DOMAIN=${cloudfront_domain}|" \
-  /tools/megazord-composition/.env; then
-  echo -e "${RED_FG}[ \U2757] Error updating CLOUDFRONT_DOMAIN in /tools/megazord-composition/.env${RESET}"
-  exit 1
+while getopts "r:d:c:h:" arg; do
+    case ${arg} in
+        h)
+            usage
+            exit;;
+        r)
+            redirect_location=${OPTARG};;
+        d)
+            domain=${OPTARG};;
+        c)
+            cloudfront_domain=${OPTARG};;
+        :)
+            echo -e "${RED_FG}[!]${RESET} $0: A value is required for -${OPTARG}" >&2
+            exit 1;;
+        ?)
+            echo -e "${RED_FG}[!]${RESET} $0: Invalid option: -${OPTARG}" >&2
+            exit 2;;
+    esac
+done
+
+if [ -z "${redirect_location}" ]; then
+    echo -e "${RED_FG}[!]${RESET}-r option is required" >&2
+    exit 1
 fi
 
-echo -e "${GREEN_FG}[\U2714] Added CLOUDFRONT_DOMAIN to .env${RESET}\n"
+if [ -z "${domain}" ]; then
+    echo -e "${RED_FG}[!]${RESET}-d option is required" >&2
+    exit 1
+fi
+
+if [ -z "${cloudfront_domain}" ]; then
+    echo -e "${RED_FG}[!]${RESET}-c option is required" >&2
+    exit 1
+fi
+
+
+# DO NOT modify the following variables
+keystore_password=$(tail -4 $original_profile | grep 'password' | cut -d ' ' -f 5)
+keystore_password=${keystore_password:1:-2}
+keystore="${domain}.store"
+keystore_path="/tools/Malleable-C2-Profiles/normal/${keystore}"
+c2_profile="${domain}-$(date '+%Y-%m-%d').profile"
+########################################
+
+
+find_and_replace () 
+{
+    original_val=$(grep $1 $3)
+    echo "[*] Updating ${1} variable in ${3}"
+
+    if ! sed -i "s|${original_val}|${1}=${2}|" ${3}; then
+        echo -e "${RED_FG}[ \U2757] Error updating ${1} in ${3}${RESET}"
+        exit 1
+    fi
+
+    echo -e "${GREEN_FG}[\U2714] Added ${1} to ${3}${RESET}\n"
+}
+
+
+# Add cloudfront_domain to env file
+find_and_replace "CLOUDFRONT_DOMAIN" $cloudfront_domain "/tools/megazord-composition/.env"
 
 # Add c2_domain to env file
-
-echo "[*] Updating C2_DOMAIN variable in /tools/megazord-composition/.env"
-original_val=$(grep 'C2_DOMAIN' /tools/megazord-composition/.env)
-
-if ! sed -i "s|${original_val}|C2_DOMAIN=${domain}|" \
-  /tools/megazord-composition/.env; then
-  echo -e "${RED_FG}[ \U2757] Error updating C2_DOMAIN in /tools/megazord-composition/.env${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Added C2_DOMAIN to .env${RESET}\n"
+find_and_replace "C2_DOMAIN" ${domain} "/tools/megazord-composition/.env"
 
 # Update keystore name in .env
-original_keystore=$(grep 'KEYSTORE' \
-  < /tools/megazord-composition/.env | cut -d '=' -f 2)
+find_and_replace "KEYSTORE" ${keystore} "/tools/megazord-composition/.env"
 
-echo -e "[*] Adding keystore name to .env"
-
-if ! sed -i "s|${original_keystore}|${keystore}|" /tools/megazord-composition/.env; then
-  echo -e "${RED_FG}[ \U2757] Error updating keystore name in .env${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Added keystore to .env${RESET}\n"
 
 # Extract certificate for domain from keystore
 echo -e "[*] Extracting ssl certificate"
@@ -120,18 +176,7 @@ echo -e "${GREEN_FG}[\U2714] C2 Profile generated at\
  /opt/cobaltstrike/${c2_profile}${RESET}\n"
 
 # Update C2_PROFILE variable in .env file
-echo "[*] Adding c2 profile name to .env"
-
-original_profile=$(grep 'C2_PROFILE' \
-  < /tools/megazord-composition/.env | cut -d '=' -f 2)
-
-# replace original profile name in .env with new profile name
-if ! sed -i "s|${original_profile}|${c2_profile}|" /tools/megazord-composition/.env; then
-  echo -e "${RED_FG}[ \U2757] Error adding new c2 profile name to .env${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Added c2 profile name to .env${RESET}\n"
+find_and_replace "C2_PROFILE" ${c2_profile} "/tools/megazord-composition/.env"
 
 # Generate new .htaccess based on fresh C2 Profile
 echo "[*] Generating .htaccess based on c2_profile"
@@ -167,7 +212,7 @@ echo -e "${GREEN_FG}[\U2714] Corefile created at\
 # Generate pseudo-random string to use as directory for payload hosting
 echo "[*] Renaming uploads directory with pseudo-random string"
 
-endpoint="/$(openssl rand -hex 6)/panda"
+endpoint="/$(openssl rand -hex 6)/$(openssl rand -hex 3)"
 new_line="Alias ${endpoint} \"/var/www/uploads\""
 
 uploads=$(grep 'Alias' \
@@ -184,47 +229,5 @@ echo -e "${MAGENTA_FG}https://${cloudfront_domain}${endpoint}/NAME_OF_PAYLOAD${R
 echo -e "\nPayload also accessible at ${MAGENTA_FG}https://${domain}${endpoint}/NAME_OF_PAYLOAD${RESET}\n"
 
 # Update PAYLOAD_DIR variable in .env with updated payload directory
-original_dir=$(grep 'PAYLOAD_DIR' \
-  < /tools/megazord-composition/.env | cut -d '=' -f 2)
+find_and_replace "PAYLOAD_DIR" ${endpoint} "/tools/megazord-composition/.env"
 
-if ! sed -i "s|${original_dir}|${endpoint}|" /tools/megazord-composition/.env; then
-  echo -e "${RED_FG}[ \U2757] Error adding new payload directory name to .env${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Updated PAYLOAD_DIR in .env to new directory name${RESET}\n"
-
-cna_dir=/tools/megazord-composition/cnas/server_cnas/
-
-# Update c2_domain, cloudfront_domain, and payload_dir variables in megazord.cna
-
-echo "[*] Updating cloudfront_domain variable in megazord.cna"
-original_val=$(grep 'cloudfront_domain = \"' /tools/megazord-composition/cnas/server_cnas/megazord.cna)
-
-if ! sed -i "s|${original_val}|\$cloudfront_domain = \"${cloudfront_domain}\";|" \
-  ${cna_dir}megazord.cna; then
-  echo -e "${RED_FG}[ \U2757] Error updating cloudfront_domain in megazord.cna${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Updated cloudfront_domain in megazord.cna${RESET}\n"
-
-echo "[*] Updating c2_domain variable in megazord.cna"
-original_val=$(grep 'c2_domain =' ${cna_dir}megazord.cna)
-if ! sed -i "s|${original_val}|\$c2_domain = \"${domain}\";|" \
-  ${cna_dir}megazord.cna; then
-  echo -e "${RED_FG}[ \U2757] Error updating c2_domain in megazord.cna${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Updated c2_domain in megazord.cna${RESET}\n"
-
-echo "[*] Updating payload_dir variable in megazord.cna"
-original_val=$(grep 'payload_dir = \"' ${cna_dir}megazord.cna)
-if ! sed -i "s|${original_val}|\$payload_dir = \"${endpoint}\";|" \
-  ${cna_dir}megazord.cna; then
-  echo -e "${RED_FG}[ \U2757] Error updating payload_dir in megazord.cna${RESET}"
-  exit 1
-fi
-
-echo -e "${GREEN_FG}[\U2714] Updated payload_dir in megazord.cna${RESET}\n"
